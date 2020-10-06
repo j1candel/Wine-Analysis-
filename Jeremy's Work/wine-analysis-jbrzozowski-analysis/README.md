720395e70c930ae6ab12f3319a89dbb56d0bba80
Jovany Candelario <jovany.candel@gmail.com>
2020-10-06 11:56:45 -0700

    adjustment to repo

---
+++ Jeremy's Work/wine-analysis-jbrzozowski-analysis/README.md
@@ -0,0 +1,2 @@
+# wine-analysis
+We look to analyze the wine from different regions across the world 
---
+++ Rawaf's Work/Rawafs Work/api_config.py
@@ -0,0 +1,8 @@
+# OpenWeatherMap API Key
+weather_api_key = "YOUR CODE GOES HERE"
+
+# Google API Key
+g_key = "YOUR CODE GOES HERE"
+
+# OpenCageData
+api_key = "YOUR CODE GOES HERE"
